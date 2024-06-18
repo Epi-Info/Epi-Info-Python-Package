4ddb061aff3ca97c9bbbc00775a35939098a126c
John Copeland <zfj4@cdc.gov>
2024-06-18 11:46:57 -0400

    Added MakeSQLite class

---
+++ build/lib/epiinfo/MakeSQLite.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+class MakeSQLite:
+    def makeTable(self, dbfile, ds, dsname, sqldatatypes):
+        sqlconn = sqlite3.connect(dbfile)
+        cursor = sqlconn.cursor()
+        cursor.execute("drop table if exists " + dsname)
+        createstatement = 'create table ' + dsname + '('
+        for k in ds[0]:
+            createstatement += k + ", "
+        createstatement = createstatement[:-2] + ');'
+        cursor.execute(createstatement)
+        for s in ds:
+            instmt = 'insert into ' + dsname + ' values('
+            for k in s:
+                if str(s[k]) == 'None':
+                    instmt += "NULL, "
+                elif sqldatatypes[k] == 'TEXT':
+                    instmt += "'" + str(s[k]) + "', "
+                else:
+                    instmt += str(s[k]) + ", "
+            instmt = instmt[:-2] + ');'
+            cursor.execute(instmt)
+        sqlconn.commit()
+        res = cursor.fetchall()
+        cursor.close()
+        return res
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '1.1.0.8'
+VERSION = '1.2.0.1'
 PACKAGE_NAME = 'epiinfo'
 AUTHOR = 'John Copeland (ported code to Pyton and added interaction odds ratios and log-binomial regression)'
 AUTHOR_EMAIL = 'zfj4@cdc.gov'
